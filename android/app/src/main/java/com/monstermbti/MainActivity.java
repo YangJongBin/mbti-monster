e6a4f092d41ccb9f36c8ebffb65577b01a7601c9
YangJongBin <toutopia0@gmail.com>
2021-12-07 21:13:02 +0900

    tpye script init

--- android/app/src/main/java/com/monstermbti/MainActivity.java
+++ android/app/src/main/java/com/monstermbti/MainActivity.java
@@ -1,4 +1,4 @@
-package com.mbtimonster;
+package com.monstermbti;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "mbtiMonster";
+    return "monsterMBTI";
   }
 }
