df467c555ef6354c67ca6f9ec13acba24eb7ba38
Y_JONGBIN <toutopia0@gmail.com>
2021-12-13 14:11:47 +0900

    Update index.tsx

--- src/modules/index.tsx
+++ src/modules/index.tsx
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
-import initReducer from './init';
+import init from './init';
 
 const rootReducer = combineReducers({
-  initReducer,
+  init,
 });
 
 export default rootReducer;
